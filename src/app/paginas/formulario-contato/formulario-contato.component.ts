a6fe7e21ce72cfecd4e25f9d42ec7482ba6f804d
eltonluiz178 <eltonluiz533@gmail.com>
2025-04-10 13:10:34 -0300

    feat: Implementando fotos a lista de contatos, novo contato e edição e exclusão.

--- src/app/paginas/formulario-contato/formulario-contato.component.ts
+++ src/app/paginas/formulario-contato/formulario-contato.component.ts
@@ -1,9 +1,10 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ContainerComponent } from '../../componentes/container/container.component';
 import { SeparadorComponent } from '../../componentes/separador/separador.component';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-import { RouterLink } from '@angular/router';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
+import { ContatoService } from '../../services/contato.service';
 
 @Component({
   selector: 'app-formulario-contato',
@@ -18,13 +19,25 @@ import { RouterLink } from '@angular/router';
   templateUrl: './formulario-contato.component.html',
   styleUrl: './formulario-contato.component.css'
 })
-export class FormularioContatoComponent {
+export class FormularioContatoComponent implements OnInit {
 
   contatoForm!: FormGroup;
 
-  constructor() {
+  constructor(
+    private contatoService: ContatoService,
+    private router: Router,
+    private activatedRoute: ActivatedRoute
+  ) {}
+
+  ngOnInit(){
+    this.inicializarFormulario();
+    this.carregarContato();
+  }
+
+  inicializarFormulario() {
     this.contatoForm = new FormGroup({
       nome: new FormControl('', Validators.required ),
+      avatar: new FormControl('', Validators.required ),
       telefone: new FormControl('', Validators.required),
       email: new FormControl('', [Validators.required, Validators.email]),
       aniversario: new FormControl(''),
@@ -33,13 +46,45 @@ export class FormularioContatoComponent {
     });
   }
 
-  salvarContato() {
-    if(this.contatoForm.valid){
-      console.log(this.contatoForm.value);
+  carregarContato() {
+    const id = this.activatedRoute.snapshot.paramMap.get('id')
+    if(id){
+    this.contatoService.buscarPorId(parseInt(id)).subscribe((contato) => {
+      this.contatoForm.patchValue(contato)
+    });
     }
   }
 
+  salvarContato() {
+      const novoContato = this.contatoForm.value;
+      const id = this.activatedRoute.snapshot.paramMap.get('id')
+      novoContato.id = id ? parseInt(id) : null;
+
+      this.contatoService.editarOuSalvarContato(novoContato).subscribe(() => {
+        this.contatoForm.reset();
+        this.router.navigateByUrl('/lista-contatos');
+      });
+  }
+
+  aoSelecionarArquivo(event: any){
+    const file: File = event.target.files[0];
+    if(file){
+      this.lerArquivo(file)
+    }
+  }
+
+  lerArquivo(file: File){
+    const reader = new FileReader();
+    reader.onload = () => {
+      if(reader.result){
+        this.contatoForm.get('avatar')?.setValue(reader.result)
+      }
+    }
+    reader.readAsDataURL(file)
+  }
+
   cancelar() { 
-    console.log("Submissão cancelada")
+    this.contatoForm.reset();
+    this.router.navigateByUrl('/lista-contatos');
   }
 }
--- src/app/paginas/lista-contatos/lista-contatos.component.ts
+++ src/app/paginas/lista-contatos/lista-contatos.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ContainerComponent } from '../../componentes/container/container.component';
 import { CabecalhoComponent } from '../../componentes/cabecalho/cabecalho.component';
 import { SeparadorComponent } from '../../componentes/separador/separador.component';
@@ -7,36 +7,39 @@ import { FormsModule } from '@angular/forms';
 import { FormularioContatoComponent } from '../formulario-contato/formulario-contato.component';
 import { CommonModule } from '@angular/common';
 
-interface Contato {
-  id: number
-  nome: string
-  telefone: string
-}
-
-import agenda from '../../agenda.json'
 import { RouterLink } from '@angular/router';
+import { ContatoService } from '../../services/contato.service';
+import { Contato } from '../../componentes/contato/contato';
 
 @Component({
   selector: 'app-lista-contatos',
   standalone: true,
   imports: [
     CommonModule,
-    ContainerComponent, 
-    CabecalhoComponent, 
+    ContainerComponent,
+    CabecalhoComponent,
     SeparadorComponent,
     ContatoComponent,
     FormsModule,
     FormularioContatoComponent,
     RouterLink
-  ],
+],
   templateUrl: './lista-contatos.component.html',
   styleUrl: './lista-contatos.component.css'
 })
-export class ListaContatosComponent {
+export class ListaContatosComponent implements OnInit{
   alfabeto: string = 'abcdefghijklmnopqrstuvwxyz'
-  contatos: Contato[] = agenda
+  contatos: Contato[] = [];
 
   filtroPorTexto: string = ''
+
+  constructor(private contatoService: ContatoService) {}
+
+  ngOnInit() {
+  this.contatoService.obterContatos().subscribe(listaContatos => {
+    this.contatos = listaContatos;
+  });
+  }
 
   removerAcentos(texto: string): string{
     return texto.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
